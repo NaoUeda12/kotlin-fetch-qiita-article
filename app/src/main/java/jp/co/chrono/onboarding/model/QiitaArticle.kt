c09eeb37104b28e8507d454fe8117d3cf510b305
nadanadana <nnuunn1224@gmail.com>
2024-11-20 13:48:59 +0900

    ボタンと検索バーのスペース追加

--- app/src/main/java/jp/co/chrono/onboarding/model/QiitaArticle.kt
+++ app/src/main/java/jp/co/chrono/onboarding/model/QiitaArticle.kt
@@ -1,23 +1,23 @@
-package jp.co.chrono.onboarding.model
-
-import jp.co.chrono.onboarding.model.QiitaArticle
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
-import retrofit2.http.GET
-
-@Serializable
-data class QiitaArticle(
-    val id: String,
-    val title: String,
-    val user: User
-)
-
-@Serializable
-data class User(
-    val id: String,
-    val name: String,
-    @SerialName("profile_image_url")
-    val profileImageUrl: String
-)
+//package jp.co.chrono.onboarding.model
+//
+//import jp.co.chrono.onboarding.model.QiitaArticle
+//import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+//import kotlinx.serialization.json.Json
+//import okhttp3.MediaType.Companion.toMediaType
+//import retrofit2.Retrofit
+//import retrofit2.http.GET
+//
+//@Serializable
+//data class QiitaArticle(
+//    val id: String,
+//    val title: String,
+//    val user: User
+//)
+//
+//@Serializable
+//data class User(
+//    val id: String,
+//    val name: String,
+//    @SerialName("profile_image_url")
+//    val profileImageUrl: String
+//)
